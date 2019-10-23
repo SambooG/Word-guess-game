f37d063906a86fb9c6755b5e253dcc11363d2c07
Samantha Gaiser <sgaiser3@gmail.com>
2019-10-23 14:30:33 -0400

    giving variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+ 
+    
+    var amyArray = ["Frank", "Valerie", "Back to Black", "Rehab"]
+    
+    var frank= ["f", "r", "a", "n", "k"];
+    if(onkeyup().includes);
+    
+    document.write 
+or
+    return 
+
+    
+    document.getElementById(frank).innerHTML = n;
+
+    var score = 0
+
+    score++
+
+    var valerie = ["v", "a", "l", "e", "r", "i", "e"];
+
+
+    var BackToBlack = ["b", "a", "c", "k", "t", "o", "b", "l", "a", "c", "k"];
+
+    
+    var rehab = ["r", "e", "h", "a", "b"]
